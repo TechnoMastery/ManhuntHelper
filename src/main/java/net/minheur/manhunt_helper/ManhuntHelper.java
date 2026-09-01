26c90710a745503920f3c4c5807c45da1d4b8400
minheur2000 <adrien.rousselet@hotmail.com>
2026-09-01 13:58:32 +0200

    added headstart config & team choice

--- src/main/java/net/minheur/manhunt_helper/ManhuntHelper.java
+++ src/main/java/net/minheur/manhunt_helper/ManhuntHelper.java
@@ -3,13 +3,19 @@ package net.minheur.manhunt_helper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.scoreboard.Team;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.WorldSavePath;
 import net.minecraft.world.World;
 import net.minecraft.world.rule.GameRules;
@@ -44,6 +50,43 @@ public class ManhuntHelper implements ModInitializer {
         CommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess, environment) -> {
             dispatcher.register(
                     literal("mh")
+                            .then(literal("team")
+                                    .then(literal("runner").executes(context -> {
+                                        if (!isTeamChoiceAllowed(context)) return 0;
+                                        ServerPlayerEntity player = context.getSource().getPlayer();
+                                        Scoreboard scoreboard = player.getEntityWorld().getScoreboard();
+                                        scoreboard.addScoreHolderToTeam(player.getName().getString(), scoreboard.getTeam("runner"));
+                                        context.getSource().sendFeedback(() -> Text.empty()
+                                                .append(Text.literal("You successfully joined ").formatted(Formatting.DARK_GREEN))
+                                                .append(Text.literal("runner").formatted(Formatting.GREEN, Formatting.BOLD))
+                                                .append(Text.literal(" team !").formatted(Formatting.DARK_GREEN)), true);
+                                        return 1;
+                                    }))
+                                    .then(literal("hunter").executes(context -> {
+                                        if (!isTeamChoiceAllowed(context)) return 0;
+                                        ServerPlayerEntity player = context.getSource().getPlayer();
+                                        Scoreboard scoreboard = player.getEntityWorld().getScoreboard();
+                                        scoreboard.addScoreHolderToTeam(player.getName().getString(), scoreboard.getTeam("hunter"));
+                                        context.getSource().sendFeedback(() -> Text.empty()
+                                                .append(Text.literal("You successfully joined ").formatted(Formatting.DARK_GREEN))
+                                                .append(Text.literal("hunter").formatted(Formatting.BLUE, Formatting.BOLD))
+                                                .append(Text.literal(" team !").formatted(Formatting.DARK_GREEN)), true);
+                                        return 1;
+                                    }))
+                                    .then(literal("spectator").executes(context -> {
+                                        if (!isTeamChoiceAllowed(context)) return 0;
+                                        ServerPlayerEntity player = context.getSource().getPlayer();
+                                        Scoreboard scoreboard = player.getEntityWorld().getScoreboard();
+                                        Team team = scoreboard.getScoreHolderTeam(player.getName().getString());
+                                        if (team != null)
+                                            scoreboard.removeScoreHolderFromTeam(player.getName().getString(), team);
+                                        context.getSource().sendFeedback(() -> Text.empty()
+                                                .append(Text.literal("You successfully became a ").formatted(Formatting.DARK_GREEN))
+                                                .append(Text.literal("spectator").formatted(Formatting.WHITE, Formatting.BOLD))
+                                                .append(Text.literal(" !").formatted(Formatting.DARK_GREEN)), true);
+                                        return 1;
+                                    }))
+                            )
                             .then(literal("admin")
                                     .requires(source -> { // requires op
                                         if (source.getEntity() instanceof ServerPlayerEntity player)
@@ -114,11 +157,23 @@ public class ManhuntHelper implements ModInitializer {
                                             .then(literal("allowChooseTeam")
                                                     .then(argument("allows", BoolArgumentType.bool())
                                                             .executes(context -> {
-                                                                if (GameDataManager.phase != GameDataManager.Phase.CONFIG) {
-                                                                    context.getSource().sendError(Text.literal("Not in config stage."));
-                                                                    return 0;
-                                                                }
+                                                                if (!isConfigStage(context)) return 0;
                                                                 GameDataManager.allowChooseTeam = BoolArgumentType.getBool(context, "allows");
+                                                                return 1;
+                                                            })
+                                                    )
+                                            )
+                                            .then(literal("headstart")
+                                                    .executes(context -> {
+                                                        if (!isConfigStage(context)) return 0;
+                                                        int headStartSeconds = GameDataManager.timerTicks / 20;
+                                                        context.getSource().sendFeedback(() -> Text.literal("Current configured headstart: " + String.valueOf(headStartSeconds)), false);
+                                                        return 1;
+                                                    })
+                                                    .then(argument("seconds", IntegerArgumentType.integer())
+                                                            .executes(context -> {
+                                                                if (!isConfigStage(context)) return 0;
+                                                                GameDataManager.timerTicks = IntegerArgumentType.getInteger(context, "seconds") *20;
                                                                 return 1;
                                                             })
                                                     )
@@ -132,5 +187,32 @@ public class ManhuntHelper implements ModInitializer {
 
     public static Path getConfigDir() {
         return FabricLoader.getInstance().getConfigDir().resolve("manhunt-helper");
+    }
+
+    private static boolean isTeamChoiceAllowed(CommandContext<ServerCommandSource> context) {
+        if (!(context.getSource().getEntity() instanceof ServerPlayerEntity))
+            return false;
+        if (GameDataManager.phase != GameDataManager.Phase.CONFIG) {
+            context.getSource().sendError(Text.literal("Need to be in config state to choose your team."));
+            return false;
+        }
+        if (!GameDataManager.allowChooseTeam) {
+            context.getSource().sendError(Text.literal("This game's host disabled team choice."));
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isConfigStage(CommandContext<ServerCommandSource> context) {
+        assert context.getSource().getEntity() != null;
+        if (!context.getSource().getEntity().getCommandTags().contains("host")) {
+            context.getSource().sendError(Text.literal("You are not this game's host!"));
+            return false;
+        }
+        if (GameDataManager.phase != GameDataManager.Phase.CONFIG) {
+            context.getSource().sendError(Text.literal("Not in config stage."));
+            return false;
+        }
+        return true;
     }
 }
